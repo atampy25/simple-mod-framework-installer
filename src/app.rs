c92fb13df98936c636584fcb077918bd09d53fbb
atampy25 <24306974+atampy25@users.noreply.github.com>
2023-04-21 18:00:38 +1000

    Switch to stable Rust

--- src/app.rs
+++ src/app.rs
@@ -53,16 +53,16 @@ struct SteamUser {
 impl App {
 	/// Called once before the first frame.
 	pub fn new() -> Self {
-		let download_size: anyhow::Result<f64> = try {
-			reqwest::blocking::Client::new()
+		let download_size = (|| -> anyhow::Result<f64> {
+			Ok(reqwest::blocking::Client::new()
 				.head(FRAMEWORK_DOWNLOAD_URL)
 				.send()?
 				.headers()
 				.get("Content-Length")
 				.context("Content-Length")?
 				.to_str()?
-				.parse()?
-		};
+				.parse()?)
+		})();
 
 		App {
 			download_size: download_size.ok(),
@@ -128,7 +128,7 @@ impl eframe::App for App {
 
 				ui.add_space(2.5);
 
-				let x: anyhow::Result<()> = try {
+				let x = (|| -> anyhow::Result<()> {
 					if !self.performed_automatic_check {
 						let mut check_paths = vec![];
 
@@ -178,8 +178,9 @@ impl eframe::App for App {
 								Ok(Data::String(d)) => {
 									for entry in fs::read_dir(d.to_string_lossy())?
 										.filter_map(|x| x.ok())
-										.filter(|x| x.file_type().unwrap().is_file())
-									{
+										.filter(|x| {
+											x.file_type().ok().map(|x| x.is_file()).unwrap_or(false)
+										}) {
 										let manifest_data: Value = serde_json::from_slice(
 											&fs::read(entry.path()).with_context(|| {
 												format!(
@@ -313,7 +314,6 @@ impl eframe::App for App {
 								Ok(_) => {
 									self.error =
 										Some("Registry key SteamPath was not string".to_owned());
-									return;
 								}
 
 								Err(_) => {}
@@ -387,20 +387,25 @@ impl eframe::App for App {
 
 						self.performed_automatic_check = true;
 					}
-				};
 
-				if let anyhow::Result::Err(x) = x {
-					self.error = Some(format!("{x:?}"));
-				}
-
-				if !self.valid_game_folders.is_empty() {
-					if self.valid_game_folders.len() == 1 {
-						ui.label(RichText::from("✅ Game folder found automatically").size(7.0));
-					} else {
-						ComboBox::from_label(RichText::from("Select your game folder").size(7.0))
+					if !self.valid_game_folders.is_empty() {
+						if self.valid_game_folders.len() == 1 {
+							ui.label(
+								RichText::from("✅ Game folder found automatically").size(7.0)
+							);
+						} else {
+							ComboBox::from_label(
+								RichText::from("Select your game folder").size(7.0)
+							)
 							.selected_text(
 								RichText::from(if let Some(x) = self.selected_game_folder {
-									String::from(self.valid_game_folders[x].0.to_string_lossy())
+									String::from(
+										self.valid_game_folders
+											.get(x)
+											.context("selected_game_folder")?
+											.0
+											.to_string_lossy()
+									)
 								} else {
 									"".to_owned()
 								})
@@ -417,19 +422,26 @@ impl eframe::App for App {
 									);
 								}
 							});
+						}
+					} else {
+						ui.label(
+							RichText::from(
+								"It doesn't look like HITMAN 3 is installed anywhere (that, or \
+								 every version already has the framework installed). Make sure \
+								 you're trying to install the framework on a copy of HITMAN 3 \
+								 installed via Steam, Epic Games Launcher/Legendary or the Xbox \
+								 app; if you can't fix this, contact Atampy26 on Hitman Forum \
+								 (note that this does not say Nexus Mods)."
+							)
+							.size(7.0)
+						);
 					}
-				} else {
-					ui.label(
-						RichText::from(
-							"It doesn't look like HITMAN 3 is installed anywhere (that, or every \
-							 version already has the framework installed). Make sure you're \
-							 trying to install the framework on a copy of HITMAN 3 installed via \
-							 Steam, Epic Games Launcher/Legendary or the Xbox app; if you can't \
-							 fix this, contact Atampy26 on Hitman Forum (note that this does not \
-							 say Nexus Mods)."
-						)
-						.size(7.0)
-					);
+
+					Ok(())
+				})();
+
+				if let Err(x) = x {
+					self.error = Some(format!("{x:?}"));
 				}
 
 				ui.add_space(5.0);
@@ -447,7 +459,7 @@ impl eframe::App for App {
 						);
 
 						ui.horizontal_wrapped(|ui| {
-							if self.selected_game_folder.is_some() {
+							if let Some(selected_game_folder) = self.selected_game_folder {
 								if self.download_promise.is_none()
 									&& ui
 										.button(RichText::from("Install the framework").size(7.0))
@@ -469,34 +481,46 @@ impl eframe::App for App {
 									});
 								}
 
-								ui.label(
-									RichText::from(
-										self.valid_game_folders[self.selected_game_folder.unwrap()]
-											.0
-											.to_str()
-											.unwrap()
-									)
-									.size(7.0)
-								);
+								let x = (|| -> anyhow::Result<()> {
+									ui.label(
+										RichText::from(
+											self.valid_game_folders
+												.get(selected_game_folder)
+												.context("selected_game_folder")?
+												.0
+												.to_str()
+												.context("game folder to_str")?
+										)
+										.size(7.0)
+									);
 
-								ui.label(
-									RichText::from(
-										if let Some(s) = &self.valid_game_folders
-											[self.selected_game_folder.unwrap()]
-										.1
-										{
-											format!("✅ Hello, {}!", s)
-										} else {
-											if self.valid_game_folders.len() == 1 {
-												"✅ Game folder found automatically"
+									ui.label(
+										RichText::from(
+											if let Some(s) = &self
+												.valid_game_folders
+												.get(selected_game_folder)
+												.context("selected_game_folder")?
+												.1
+											{
+												format!("✅ Hello, {}!", s)
 											} else {
-												"✅ Game folder selected"
+												if self.valid_game_folders.len() == 1 {
+													"✅ Game folder found automatically"
+												} else {
+													"✅ Game folder selected"
+												}
+												.to_owned()
 											}
-											.to_owned()
-										}
-									)
-									.size(7.0)
-								);
+										)
+										.size(7.0)
+									);
+
+									Ok(())
+								})();
+
+								if let Err(x) = x {
+									self.error = Some(format!("{x:?}"));
+								}
 							}
 						});
 
@@ -511,49 +535,65 @@ impl eframe::App for App {
 									if !self.installation_done {
 										match data {
 											Ok(data) => {
-												extract(
-													Cursor::new(data),
-													&self.valid_game_folders
-														[self.selected_game_folder.unwrap()]
-													.0
-													.join("Simple Mod Framework"),
-													false
-												)
-												.unwrap();
+												match (|| -> anyhow::Result<()> {
+													extract(
+														Cursor::new(data),
+														&self
+															.valid_game_folders
+															.get(
+																self.selected_game_folder.context(
+																	"selected_game_folder"
+																)?
+															)
+															.context("game folder index")?
+															.0
+															.join("Simple Mod Framework"),
+														false
+													)?;
 
-												ShellLink::new(
-													self.valid_game_folders
-														[self.selected_game_folder.unwrap()]
-													.0
-													.join("Simple Mod Framework")
-													.join("Mod Manager")
-													.join("Mod Manager.exe")
-													.to_str()
-													.unwrap()
-												)
-												.unwrap()
-												.create_lnk(
-													home_dir()
-														.unwrap()
-														.join("AppData")
-														.join("Roaming")
-														.join("Microsoft")
-														.join("Windows")
-														.join("Start Menu")
-														.join("Programs")
-														.join("Simple Mod Framework.lnk")
-												)
-												.unwrap();
+													ShellLink::new(
+														self.valid_game_folders
+															.get(
+																self.selected_game_folder.context(
+																	"selected_game_folder"
+																)?
+															)
+															.context("game folder index")?
+															.0
+															.join("Simple Mod Framework")
+															.join("Mod Manager")
+															.join("Mod Manager.exe")
+															.to_str()
+															.context("linktarget to_str")?
+													)?
+													.create_lnk(
+														home_dir()
+															.context("home dir")?
+															.join("AppData")
+															.join("Roaming")
+															.join("Microsoft")
+															.join("Windows")
+															.join("Start Menu")
+															.join("Programs")
+															.join("Simple Mod Framework.lnk")
+													)?;
 
-												self.installation_done = true;
+													Ok(())
+												})()
+												.context("Extracting/creating link")
+												{
+													Ok(_) => {
+														self.installation_done = true;
+													}
+
+													Err(e) => {
+														self.error = Some(format!("{e:?}"));
+													}
+												};
 											}
 
 											Err(e) => {
-												ui.label(
-													RichText::from(e)
-														.color(Color32::from_rgb(200, 50, 50))
-														.size(8.0)
-												);
+												self.error = Some(format!("{e:?}"));
 											}
 										}
 									}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #![warn(clippy::all, rust_2018_idioms)]
-#![feature(try_blocks)]
 
 mod app;
 pub use app::App;
